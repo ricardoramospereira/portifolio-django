e42471f08a625dac11d4137c6d962f3c14c10f78
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2022-10-27 17:21:53 -0300

    add_models

--- core/models.py
+++ core/models.py
@@ -1,9 +1,66 @@
-from unicodedata import decimal
-from unittest.util import _MAX_LENGTH
+from distutils.command.upload import upload
+from email.policy import default
+from operator import truediv
+from pyexpat import model
+from random import choices
+from tabnanny import verbose
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
-class Produto(models.Model):
+class Base(models.Model):
+    criados = models.DateField('criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=100)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.servico
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+class Funcionario(Base):
     nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
-    estoque = models.IntegerField('Quantidade em estoque')
+    cargo = models.ForeignKey('core.Cargo', verbose_name = 'Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    imagem = StdImageField('Imagem', upload_to='equipe', variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self):
+        return self.nome
+
+    
+
+    
